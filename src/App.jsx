aed7eea56f211e60414de47a3fd08af2b8b090ce
Joseph Michael Dizon <dizonjoseph23@gmail.com>
2023-10-16 12:38:14 +0800

    initial content commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import HelloWorld from "./assets/components/HelloWorld";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
